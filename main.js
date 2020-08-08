2a548bc91560400c7c4c70e772b4f0f3c9d13969
oo6xoo <65550015+oo6xoo@users.noreply.github.com>
2020-08-08 05:49:30 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function myfun(){
   let x = 1 ;
      if (true){
@@ -12,17 +13,44 @@ console.log(myfun());
 
 
 
+const SETTINGS = 1;
+if (true) {
+  const SETTINGS = 2;
+  console.log(SETTINGS);
+}
+console.log(SETTINGS);
 
 
 
 
+const SETTINGS = [];
+SETTINGS.push('html', 'css', 'javascript');
+console.log(SETTINGS);
 
 
 
 
+const SETTINGS = {
+  'name' : 'AbdAlrazzaq',
+  'age' : '28',
+};
+//SETTINGS.name = 'ahmed';
+console.log(SETTINGS);
 
 
-/*
+
+
+const SETTINGS = {
+  'name' : 'AbdAlrazzaq',
+  'age' : '28',
+};
+object.freeze(SETTINGS);
+SETTINGS.name = 'Ali';
+console.log(SETTINGS);
+
+
+
+
 let array1 = [1,2,3];
 let  array2 = [...array1];
 array2.push(66);
@@ -30,12 +58,16 @@ console.log(array1);
 console.log(array2);
 
 
+
+
 let  array3 = [1,2,3,...array2,6,7];
 console.log(array3);
+
+
 
 
 function  myfun(usrname, pasword, url){
   return `Hello ${usrname} how ar you ${pasword} wat linl ${url}`;
 }
 console.log(myfun('Abd', 'a123', 'www'));
-*/
+
