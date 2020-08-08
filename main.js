003d89e51571aead0667fa17b6087e578585bf67
oo6xoo <65550015+oo6xoo@users.noreply.github.com>
2020-08-08 04:52:31 +0300

    I learn code!
    I learn code! =)

---
+++ main.js
@@ -0,0 +1,41 @@
+function myfun(){
+  let x = 1 ;
+     if (true){
+     let x = 2;
+     console.log(x);
+     }
+  console.log(x);
+  return 'done';
+}
+console.log(myfun());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let array1 = [1,2,3];
+let  array2 = [...array1];
+array2.push(66);
+console.log(array1);
+console.log(array2);
+
+
+let  array3 = [1,2,3,...array2,6,7];
+console.log(array3);
+
+
+function  myfun(usrname, pasword, url){
+  return `Hello ${usrname} how ar you ${pasword} wat linl ${url}`;
+}
+console.log(myfun('Abd', 'a123', 'www'));
+*/
