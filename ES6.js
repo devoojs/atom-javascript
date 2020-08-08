14e02df896ff7889bb23d2017a2110a57f1ed509
oo6xoo <65550015+oo6xoo@users.noreply.github.com>
2020-08-08 13:23:21 +0300

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -8,8 +8,6 @@ function myfun(a, b) {
 console.log(myfun(10, 10));
 
 
-
-
 function sum(...theArgs) {
   return theArgs.reduce((num1, num2) => {
     return num1 + num2;
@@ -17,3 +15,95 @@ function sum(...theArgs) {
 }
 console.log(sum(1, 2, 3));
 console.log(sum(1, 2, 3, 4));
+
+let message;
+
+
+let message;
+message = 'Hello'; // store the string
+
+
+let message;
+message = 'Hello!';
+alert(message); // shows the variable content
+
+
+let message = 'Hello!'; // define the variable and assign the value
+alert(message); // Hello!
+
+
+let user = 'John', age = 25, message = 'Hello';
+
+
+let user = 'John';
+let age = 25;
+let message = 'Hello';
+
+
+let user = 'John',
+  age = 25,
+  message = 'Hello';
+
+
+  let user = 'John'
+  , age = 25
+  , message = 'Hello';
+
+
+  let message;
+message = 'Hello!';
+message = 'World!'; // value changed
+alert(message);
+
+
+let hello = 'Hello world!';
+let message;
+// copy 'Hello world' from hello into message
+message = hello;
+// now two variables hold the same data
+alert(hello); // Hello world!
+alert(message); // Hello world!
+
+
+let $ = 1; // declared a variable with the name "$"
+let _ = 2; // and now a variable with the name "_"
+alert($ + _); // 3
+
+
+//let, class, return, and function are reserved.
+
+const myBirthday = '18.04.1982';
+
+
+const myBirthday = '18.04.1982';
+myBirthday = '01.01.2001'; // error, can't reassign the const
+
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+console.log(color); // #FF7F00
+
+
+let admin, name; // can declare two variables at once
+name = "John";
+admin = name;
+alert( admin ); // "John"
+
+
+let ourPlanetName = "Earth";
+
+
+const birthday = '18.04.1982';
+const age = someCode(birthday);
+
+
+const BIRTHDAY = '18.04.1982'; // make uppercase?
+const AGE = someCode(BIRTHDAY); // make uppercase?
+
+let n = 123;
+n = 12.345;
+
