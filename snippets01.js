d6463a6aeb0a9fb9e8b1bbf33da47343f4f99ed0
oo6xoo <65550015+oo6xoo@users.noreply.github.com>
2020-08-08 13:39:57 +0300

    Update snippets01.js

--- snippets01.js
+++ snippets01.js
@@ -1,20 +1,3 @@
-function myfun(a, b) {
-    if (a === b) {
-        return "Different!";
-    } else {
-        return "Equal!"
-    }
-}
-console.log(myfun(10, 10));
-
-
-function sum(...theArgs) {
-  return theArgs.reduce((num1, num2) => {
-    return num1 + num2;
-  });
-}
-console.log(sum(1, 2, 3));
-console.log(sum(1, 2, 3, 4));
 
 let message;
 
